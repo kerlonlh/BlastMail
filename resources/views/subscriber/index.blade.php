018b6b4442e2ee0fa721c424f9ba1d1f8e2f71f9
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2025-08-11 21:28:23 -0300

    Refatorando blade components

--- resources/views/subscriber/index.blade.php
+++ resources/views/subscriber/index.blade.php
@@ -9,24 +9,15 @@
     <x-card class="space-y-4">
 
         <div class="flex justify-between">
-            <x-link-button :href="route('subscribers.index', $emailList)">
+            <x-button.link :href="route('subscribers.index', $emailList)">
                 {{ __('Add a new subscriber') }}
-            </x-link-button>
+            </x-button.link>
 
-            <x-form :action="route('subscribers.index', $emailList)" class="w-2/5" x-data x-ref="form">
+            <x-form :action="route('subscribers.index', $emailList)" x-data x-ref="form" class="w-3/5 flex space-x-4 items-center" flat>
+                <x-input.checkbox value="1" name="showTrash" :label="__('Show Deleted Records')" @click="$refs.form.submit()"
+                    :checked="$showTrash" />
 
-                <label for="show_trash" class="inline-flex items-center">
-
-                    <input id="show_trash" type="checkbox" value="1" @click="$refs.form.submit()"
-                        @if ($showTrash) checked @endif
-                        class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600"
-                        name="showTrash">
-
-                    <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Show Deleted Records') }}</span>
-
-                </label>
-
-                <x-text-input name="search" :placeholder="__('Search')" :value="$search" />
+                <x-input.text name="search" :placeholder="__('Search')" :value="$search" class="w-full" />
 
             </x-form>
 
@@ -42,15 +33,14 @@
                             @unless ($subscriber->trashed())
                                 <x-form :action="route('subscribers.destroy', [$emailList, $subscriber])" delete flat
                                     onsubmit="return confirm('{{ __('Are you sure you want to delete this subscriber?') }}')">
-                                    <x-secondary-button type="submit">
+                                    <x-button.secondary type="submit">
                                         {{ __('Delete') }}
-                                    </x-secondary-button>
+                                    </x-button.secondary>
                                 </x-form>
                             @else
-                                <span
-                                    class="w-fit inline-flex overflow-hidden rounded-sm border border-red-500 bg-white text-xs font-medium text-red-500 dark:border-red-500 dark:bg-neutral-950 dark:text-red-500">
-                                    <span class="px-2 py-1 bg-red-500/10 dark:bg-red-500/10">Deleted</span>
-                                </span>
+                                <x-badge danger>
+                                    {{ __('Delete') }}
+                                </x-badge>
                             @endunless
 
                         </x-table.td>
