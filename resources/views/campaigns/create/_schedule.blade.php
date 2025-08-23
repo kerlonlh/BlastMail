be4ca20af68450ac40f5361fc5fd994354d0aa64
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2025-08-23 13:10:09 -0300

    Adicionando informações de envio

--- resources/views/campaigns/create/_schedule.blade.php
+++ resources/views/campaigns/create/_schedule.blade.php
@@ -2,12 +2,14 @@
 
     <x-alert success :title="__('You campaign is ready to be send!')" />
 
-    <div>
-
-        <div>Assunt: {{ $data['subject'] }}</div>
+    <div class="space-y-2">
+        <div>{{ __('From') }}: {{ config('mail.from.address') }}</div>
+        <div>{{ __('To') }}: <x-badge>{{ $countEmails }} Emails</x-badge></div>
+        <div>{{ __('Subject') }}: {{ $data['subject'] }}</div>
+        <div>{{ __('Template') }}: <x-badge>{{ $template }}</x-badge></div>
     </div>
 
-    <hr>
+    <hr class="my-3 opacity-20">
 
     <div x-data="{ show: '{{ data_get($data, 'send_when', 'now') }}' }">
         <x-input-label :value="__('Schedule delivery')" />
