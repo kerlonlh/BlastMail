0b54fe527a3daa0bc1759469a7e299017b21977f
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2025-08-27 20:21:32 -0300

    Ajustes na renomeação do arquivo

--- routes/web.php
+++ routes/web.php
@@ -45,8 +45,6 @@ Route::middleware('auth')->group(function () {
     //region Campaigns
     Route::get('/campaigns', [CampaignController::class, 'index'])->name('campaigns.index');
 
-    Route::get('/campaigns/{campaign}/{what?}', [CampaignController::class, 'show'])->name('campaigns.show');
-
     Route::get('/campaigns/create/{tab?}', [CampaignController::class, 'create'])
         ->middleware(CampaignCreateSessionControl::class)
         ->name('campaigns.create');
@@ -55,6 +53,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/campaigns/{campaign}/restore', [CampaignController::class, 'restore'])->withTrashed()->name('campaigns.restore');
 
     Route::delete('/campaigns/{campaign}', [CampaignController::class, 'destroy'])->name('campaigns.destroy');
+
+    Route::get('/campaigns/{campaign}/{what?}', [CampaignController::class, 'show'])->name('campaigns.show');
 
 
     //endregion
